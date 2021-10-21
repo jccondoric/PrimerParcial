6bdca98da6c8a25912f46bb498a1d69bb6ec508a
Jhon Carlos Condori Cuevas <www.hackdcjcc@hotmail.com>
2021-10-20 22:09:08 -0400

    Agregar archivos de proyecto.

---
+++ PBomberManUSFX/Tile.cpp
@@ -0,0 +1,28 @@
+#include "Tile.h"
+
+Tile::Tile()
+{
+	posicionTileX = 0;
+	posicionTileY = 0;
+	bomberman = nullptr;
+	muroCeramica = nullptr;
+	sueloCesped = nullptr;
+	muroLadrillo = nullptr;
+	muroGalactico = nullptr;
+	muroMetal = nullptr;
+	sueloCircuito = nullptr;
+	
+}
+
+Tile::Tile(int _posicionTileX, int _posicionTileY)
+{
+	posicionTileX = _posicionTileX;
+	posicionTileY = _posicionTileY;
+	bomberman = nullptr;
+	muroCeramica = nullptr;
+	sueloCesped = nullptr;
+	muroLadrillo = nullptr;
+	muroMetal = nullptr;
+	muroGalactico = nullptr;
+	sueloCircuito = nullptr;
+}
